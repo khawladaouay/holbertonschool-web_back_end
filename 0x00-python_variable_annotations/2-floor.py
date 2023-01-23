248bda8dfe08a852c9a9cf2acc71c306e90386b9
khawladaouay <daouaykhawla872@gmail.com>
2023-01-23 21:39:20 +0100

    task

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task2"""
+import math
+
+
+def floor(n: float) -> int:
+    """return the floor of n"""
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""task3"""
+
+
+def to_str(n: float) -> str:
+    """return the string of n"""
+    return str(n)
