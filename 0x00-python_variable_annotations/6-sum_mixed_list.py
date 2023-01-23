fb199127fa365172d091a25a244d1ac9217db018
khawladaouay <daouaykhawla872@gmail.com>
2023-01-23 21:59:19 +0100

    task

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task6"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return the sum of a list of floats"""
+    return sum(mxd_lst)
