55dd2b59864ad6927b23cd2bf8f124d8a61fe512
khawladaouay <daouaykhawla872@gmail.com>
2023-01-27 20:53:48 +0100

    task1

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -19,12 +19,12 @@ class FIFOCache(BaseCaching):
                 return
 
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                item_discarded = self.key_indexes.pop(0)
+                item_discarded = self.keys.pop(0)
                 del self.cache_data[item_discarded]
                 print("DISCARD:", item_discarded)
 
             self.cache_data[key] = item
-            self.key_indexes.append(key)
+            self.keys.append(key)
 
     def get(self, key):
         """return the value in self.cache_data linked to key"""
