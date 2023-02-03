c2e1b5b7f8a31e094d1e4589b7d69acb3ef4252a
khawladaouay <daouaykhawla872@gmail.com>
2023-02-03 21:07:43 +0100

    all

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" encrypt passwords """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Encrypting passwords """
+    p = password.encode()
+    return bcrypt.hashpw(p, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ validate that the provided password matches the hashed password """
+    return bcrypt.checkpw(password.encode(), hashed_password)
---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import re
+from typing import List
+import logging
+import os
+import mysql.connector
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ init """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    for field in fields:
+        message = re.sub(fr'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """ returns a logger """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ returns connector """
+    return mysql.connector.connect(
+        host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.environ.get('PERSONAL_DATA_DB_NAME', 'root'),
+        user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
+        password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''))
+
+
+def main() -> None:
+    """ Main Function """
+    db = get_db()
+    cursor = db.cursor()
+    logger = get_logger()
+    cursor.execute("SELECT * FROM users;")
+    fieldcnt = len(cursor.description)
+    values = [i[0] for i in cursor.description]
+    for row in cursor:
+        toLog = ''
+        for item in range(fieldcnt):
+            toLog += f'{values[item]}={str(row[item])};'
+        logger.info(toLog)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
