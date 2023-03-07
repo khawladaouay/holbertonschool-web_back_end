3eae8561390c6306d8d8693a18e8b889f8d67701
khawladaouay <daouaykhawla872@gmail.com>
2023-03-07 22:12:50 +0100

    update

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,6 +12,11 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[
             str, bytes, int, float]:
         data = self._redis.get(key)
