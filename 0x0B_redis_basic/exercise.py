7f8505e7dac9c0ac517659c059eb6f8db939ce5f
khawladaouay <daouaykhawla872@gmail.com>
2023-03-07 13:01:20 +0100

    up

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Redis"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    """Cache class"""
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis = redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
