a42a45e12bc327b67b38c35f9210775e08abf0cd
khawladaouay <daouaykhawla872@gmail.com>
2023-03-23 11:18:39 +0100

    class

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,31 +1,34 @@
 export default class HolbertonCourse {
   constructor (name, length, students) {
-    this._name = name
-    this._length = length
-    this._students = students
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name () {
-    return this._name
+    return this._name;
   }
 
   get length () {
-    return this._length
+    return this._length;
   }
 
   get students () {
-    return this._students
+    return this._students;
   }
 
   set name (name) {
-    this._name = name
+    this.constructor.verify(name);
+    this._name = name;
   }
 
   set length (length) {
-    this._length = length
+    this.constructor.verify(null, length);
+    this._length = length;
   }
 
   set students (students) {
-    this._students = students
+    this.constructor.verify(null, null, students);
+    this._students = students;
   }
 }
